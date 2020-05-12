9944d2fadbd5e5f424f07cf4cc544c0b13560fc4
Holly Davidson <holly.davidson@imcva.com>
2020-05-12 13:45:24 -0400

    adding a timeout

--- get.js
+++ get.js
@@ -7,10 +7,9 @@ export const main = handler(async (event, context) => {
     // 'Key' defines the partition key and sort key of the item to be retrieved
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
-	//faulty code test
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      // noteId: event.pathParameters.id
+      noteId: event.pathParameters.id
     }
   };
 
@@ -18,7 +17,10 @@ export const main = handler(async (event, context) => {
   if ( ! result.Item) {
     throw new Error("Item not found.");
   }
-  
+
+  // Set a timeout
+  await new Promise(resolve => setTimeout(resolve, 10000));
+
   // Return the retrieved item
   return result.Item;
 });
